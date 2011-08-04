b300e48d6af574d29c3fe48cbcd51f9e80d4a991
Jonathan MERCIER <bioinfornatics@gmail.com>
2011-08-04 18:29:40 +0200

    perform code style pylint score: 9.57

--- Python/run_scanning.py
+++ Python/run_scanning.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
+
+"""
+run_scanning is a script for get sequence over represented on  whole genome. The sequences are selected by relation
+with a Position Weigth Matrix ( pwm ) and result are filtered by using False discovery method. The results are send
+to Genome Data Viewer ( GDV ).
+"""
+
 from bbcflib.motif              import sqlite_to_false_discovery_rate
-from bein                       import MiniLIMS, unique_filename_in, ProgramOutput, program, execution
+from bein                       import MiniLIMS, unique_filename_in, execution
 from bbcflib.genrep             import GenRep
 from bbcflib.gdv                import create_gdv_project, add_gdv_track, get_project_id
 from bbcflib.frontend           import parseConfig
 from bbcflib.common             import scp, normalize_url
 from bbcflib.track              import Track, new
-from os.path                    import basename, expanduser, normcase, splitext, isfile, exists
+from os.path                    import basename, expanduser, normcase, isfile, exists
 from os                         import sep
-import getopt, sys
+import getopt, sys, urllib2
 
-usage = """%s [-h] [options] --matrix=/path/to/matrix.mat -c config_file --minilims=minilims_name
+USAGE = """%s [-h] [options] --matrix = /path/to/matrix.mat -c config_file --minilims = minilims_name
 -h Print this message and exit
 -c --config file Config file
 --host submit track file to selected host machine (e.g sugar)
@@ -22,16 +29,21 @@ usage = """%s [-h] [options] --matrix=/path/to/matrix.mat -c config_file --minil
 --via via Run executions using method "via" (can be "local" or "lsf")
 --username ssh username for login to host
 --website website were result will be taken for GDV (e.g http://sugar.epfl.ch)
-""" %(sys.argv[0])
+""" % (sys.argv[0])
 
 class Usage(Exception):
+    """Display to user all parameter availabe with associated description"""
     def __init__(self, msg):
+        super(Usage, self).__init__()
         self.msg = msg
 
 def main(argv = None):
+    """
+    Entry point when program start
+    """
     genrep              = None
     assembly            = None
-    M                   = None
+    lims                = None
     job                 = None
     config              = None
     config_file         = None
@@ -52,54 +64,56 @@ def main(argv = None):
     limspath            = ""
     fdr                 = 0
     runs                = {}
-    if argv is None: argv = sys.argv
+    if argv is None:
+        argv = sys.argv
     try:
         try:
-            opts,args = getopt.getopt   (
+            opts, args = getopt.getopt   (
                                             argv[1:],"hu:c:"  ,
                                             [
-                                                "help", "via=", "host="     ,
-                                                "remote_path=" , "website=" ,
-                                                "minilims=","config="       ,
-                                                "matrix=", "username="      ,
-                                                "identity_file=", "project="
+                                                "help", "via = ", "host = "     ,
+                                                "remote_path = " , "website = " ,
+                                                "minilims = ","config = "       ,
+                                                "matrix = ", "username = "      ,
+                                                "identity_file = ", "project = "
                                             ]
                                         )
         except getopt.error, msg:
             raise Usage(msg)
-        for o, a in opts:
-            if o in ("-h", "--help"):
+        for option, value in opts:
+            if option in ("-h", "--help"):
                 print __doc__
-                print usage
-                sys.sys.exit(0)
-            elif o == "--via":
-                if a=="local":
+                print USAGE
+                sys.exit(0)
+            elif option == "--via":
+                if value == "local":
                     via = "local"
-                elif a=="lsf":
+                elif value == "lsf":
                     via = "lsf"
                 else:
-                    raise Usage("Via (-u) can only be \"local\" or \"lsf\", got %s." % (a,))
-            elif o == "--website":
-                website = normalize_url(a)
-            elif o == "--minilims":
-                limspath = normcase(expanduser(a))
-            elif o == "--host":
-                host = a
-            elif o == "--identity_file":
-                identity_file = a
-            elif o == "--remote_path":
-                remote_path = normcase(expanduser(a))
-                if not remote_path.endswith(sep): remote_path += sep
-            elif o == "--matrix":
-                matrix = {basename(a):normcase(expanduser(a))}
-            elif o == "--username":
-                username = a
-            elif o == "--project":
-                project = a
-            elif o in ("-c", "--config"):
-                config_file = normcase(expanduser(a))
+                    raise Usage("Via (-u) can only be \"local\" or \"lsf\", got %s." % (value,))
+            elif option == "--website":
+                website = normalize_url(value)
+            elif option == "--minilims":
+                limspath = normcase(expanduser(value))
+            elif option == "--host":
+                host = value
+            elif option == "--identity_file":
+                identity_file = value
+            elif option == "--remote_path":
+                remote_path = normcase(expanduser(value))
+                if not remote_path.endswith(sep):
+                    remote_path += sep
+            elif option == "--matrix":
+                matrix = {basename(value):normcase(expanduser(value))}
+            elif option == "--username":
+                username = value
+            elif option == "--project":
+                project = value
+            elif option in ("-c", "--config"):
+                config_file = normcase(expanduser(value))
             else:
-                raise Usage("Unhandled option: " + o)
+                raise Usage("Unhandled option: " + option)
 
         # read config file
         if config_file is None or not exists(config_file) or not isfile(config_file):
@@ -107,22 +121,24 @@ def main(argv = None):
         else:
             job, config = parseConfig(normcase(expanduser(config_file)))
 
-        if project == "": project = job.description
+        if project == "":
+            project = job.description
         if matrix == "":
             if "matrix" in job.options:
-                matrix = {basename(a):normcase(expanduser(job.options["matrix"]))}
+                path = normcase(expanduser(job.options["matrix"]))
+                matrix = {basename(path): path}
             else:
-                raise Usage("You need give a matrix file ")
+                raise Usage("You need give value matrix file ")
         if limspath == "":
             if "minilims" in job.options:
                 limspath = job.options["minilims"]
             else:
-                raise Usage("You need give a minilims path/name")
+                raise Usage("You need give value minilims path/name")
         if via == "":
             if "via" in job.options:
                 via = job.options["via"]
             else:
-                via="lsf"
+                via = "lsf"
         if host == "" and "host" in job.options:
             host = job.options["host"]
         if identity_file == "" and "identity_file" in job.options:
@@ -134,9 +150,9 @@ def main(argv = None):
         if website == "" and "website" in job.options:
             website = job.options["website"]
 
-        genrep      = GenRep(config=config)
+        genrep      = GenRep(config = config)
         assembly    = genrep.assembly(job.assembly_id)
-        M           = MiniLIMS(limspath)
+        lims           = MiniLIMS(limspath)
         json        = create_gdv_project(
                                             config["gdv"]["key"], config["gdv"]["email"],
                                             project,
@@ -147,34 +163,40 @@ def main(argv = None):
         project_id  = get_project_id( json )
 
         # compute false discovery rate
-        with execution(M, description=job.description) as ex:
-            background = genrep.statistics(assembly,output=unique_filename_in(), frequency=True, matrix_format=True)
-            ex.add(background,  description="background:"+background)
+        with execution(lims, description = job.description) as ex:
+            background = genrep.statistics  (
+                                                assembly,
+                                                output = unique_filename_in(),
+                                                frequency = True,
+                                                matrix_format = True
+                                            )
+            ex.add(background,  description = "background:"+background)
             if len(job.groups) >2:
                 raise ValueError("They are more than 2 group in config file")
 
             for group_number in job.groups:
                 group = job.groups[group_number]
                 for run_number in group["runs"]:
-                    r = job.groups[group_number]["runs"][run_number]
-                    if "url" in r:
-                        url = r["url"]
+                    run_iter = job.groups[group_number]["runs"][run_number]
+                    if "url" in run_iter:
+                        url = run_iter["url"]
                         uri = ""
-                        if r["run"] not in runs:
-                            runs[r["run"]] = {"control":None, "experimental":None}
+                        if run_iter["run"] not in runs:
+                            runs[run_iter["run"]] = {"name":None, "control":None, "experimental":None}
                         if url.startswith("http") or url.startswith("www."):
                             url = normalize_url(url)
                             # download data
                             data    = urllib2.urlopen(url)
                             uri     = unique_filename_in()
-                            with open(run["experimental"], "w") as f:
-                                f.write(data.read())
+                            with open(uri, "w") as opening_file:
+                                opening_file.write(data.read())
                         else:
-                           uri = normcase(expanduser(url))
+                            uri = normcase(expanduser(url))
                         if group["control"]:
-                            runs[r["run"]]["control"] = uri
+                            runs[run_iter["run"]]["control"]   = uri
+                            runs[run_iter["run"]]["name"]      = basename(uri)
                         else:
-                            runs[r["run"]]["experimental"] = uri
+                            runs[run_iter["run"]]["experimental"] = uri
 
             for run in runs:
                 current_run         = runs[run]
@@ -183,26 +205,28 @@ def main(argv = None):
                 track_filtered      = unique_filename_in()
 
                 # convert data to sql
-                with Track(current_run["experimental"], chrmeta=assembly.chromosomes) as track:
+                with Track(current_run["experimental"], chrmeta = assembly.chromosomes) as track:
                     # Get sqlite file if is not arleady in this format
                     if track.format != "sql" or track.format != "db" or track.format != "sqlite":
-                        track.convert(original_sql_data, format="sql")
+                        track.convert(original_sql_data, format = "sql")
                     else:
                         original_sql_data = current_run["experimental"]
                     # Generate a random population from orginal if it is not give from config file
                     if current_run["control"] is None:
                         # create random track
-                        track.shuffle_track(random_sql_data, repeat_number=5)
+                        track.shuffle_track(random_sql_data, repeat_number = 5)
                     else:
-                        with Track(current_run["control"], chrmeta=assembly.chromosomes) as track_random:
+                        with Track(current_run["control"], chrmeta = assembly.chromosomes) as track_random:
                             # Get sqlite file if is not arleady in this format
-                            if track_random.format != "sql" or track_random.format != "db" or track_random.format != "sqlite":
-                                track_random.convert(random_sql_data, format="sql")
+                            if track_random.format != "sql" or \
+                                track_random.format != "db" or \
+                                track_random.format != "sqlite":
+                                track_random.convert(random_sql_data, format = "sql")
                             else:
                                 random_sql_data = current_run["control"]
                 ex.add(original_sql_data,   "sql:"+original_sql_data)
                 ex.add(random_sql_data,     "sql:"+random_sql_data)
-                track_scanned,fdr = sqlite_to_false_discovery_rate(
+                track_scanned, fdr = sqlite_to_false_discovery_rate(
                                                                     ex,
                                                                     matrix,
                                                                     background,
@@ -210,23 +234,26 @@ def main(argv = None):
                                                                     assembly.chromosomes,
                                                                     original_sql_data,
                                                                     random_sql_data,
-                                                                    threshold=0,
-                                                                    via=via,
-                                                                    keep_max_only=True,
-                                                                    alpha=0.05,
-                                                                    nb_false_positive_hypotesis=5.0
+                                                                    threshold = 0,
+                                                                    via = via,
+                                                                    keep_max_only = True,
+                                                                    alpha = 0.05,
+                                                                    nb_false_positive_hypotesis = 5.0
                                                                   )
 
                 # filter track with fdr as treshold
-                with new(track_filtered, format="sql", datatype="qualitative") as track_out:
+                with new(track_filtered, format = "sql", datatype = "qualitative") as track_out:
                     chromosome_used     = {}
-                    track_out.meta_track= {"source": basename(current_run["experimental"])}
+                    track_out.meta_track = {"source": basename(current_run["experimental"])}
                     track_out.meta_track.update({"k":"v"})
-                    with Track(track_scanned, format="sql", chrmeta=assembly.chromosomes) as track_in:
-                        meta = dict([(v["name"],dict([("length",v["length"])])) for v in track_in.chrmeta.values()])
+                    with Track(track_scanned, format = "sql", chrmeta = assembly.chromosomes) as track_in:
+                        meta = dict([(v["name"], dict([("length", v["length"])])) for v in track_in.chrmeta.values()])
                         for chromosome in track_in.all_chrs:
                             data_list = []
-                            for data in track_in.read( {"chr": chromosome, "score": (fdr, sys.maxsize)}, fields=Track.qualitative_fields ):
+                            for data in track_in.read   (
+                                                            {"chr": chromosome, "score": (fdr, sys.maxsize)},
+                                                            fields = Track.qualitative_fields
+                                                        ):
                                 data_list.append(data)
                                 chromosome_used[chromosome] = meta[chromosome]
                             if len(data_list) > 0:
@@ -240,20 +267,21 @@ def main(argv = None):
                     if identity_file != "":
                         args = ["-i", normcase(expanduser(identity_file)), "-C" ]
                     source      = normcase(expanduser(track_filtered))
-                    destination = "%s@%s:%s%s%s.db" %(username, host, remote_path, sep, track_filtered)
-                    result_path = "%s%s%s.db" %(website, sep, track_filtered)
-                    scp(ex, source, destination, args=args)
+                    destination = "%s@%s:%s%s%s.db" % (username, host, remote_path, sep, track_filtered)
+                    result_path = "%s%s%s.db" % (website, sep, track_filtered)
+                    scp(ex, source, destination, args = args)
                 else:
                     result_path = track_filtered
 
                 add_gdv_track  (
                                     config["gdv"]["key"], config["gdv"]["email"],
                                     project_id, result_path,
-                                    gdv_url=config["gdv"]["url"]
+                                    name    = current_run["name"],
+                                    gdv_url = config["gdv"]["url"]
                                 )
     except Usage, err:
-        print >>sys.stderr, err.msg
-        print >>sys.stderr, usage
+        print >> sys.stderr, err.msg
+        print >> sys.stderr, USAGE
         return 2
 if __name__ == "__main__":
     sys.exit(main())
