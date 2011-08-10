b988920daec0921562a657585a9979a0b018629a
Julien Delafontaine <julien.delafontaine@epfl.ch>
2011-08-10 17:24:29 +0200

    run_deseq modified consistently with rnaseq.py changes

--- Python/run_deseq.py
+++ Python/run_deseq.py
@@ -1,4 +1,4 @@
-#!/archive/epfl/bbcf/bin/bin.x86_64/python
+#!/bin/env python
 import getopt
 import os
 import sys
@@ -10,8 +10,13 @@ import rpy2.robjects.packages as rpackages
 import rpy2.robjects.numpy2ri
 import rpy2.rlike.container as rlc
 
-usage = """run_deseq.py cond1_file cond2_file transcript_names_file cond1_label cond2_label method assembly_id [output]
+usage = """run_deseq.py cond1_file cond2_file cond1_label cond2_label method [output] [gene_names]
 
+*cond1_file*, *cond2_file*:     arrays of counts
+*cond1_label*, *cond2_label* :  string describing the respective condition
+*method*:                       'normal' or 'blind'
+*output*:                       name of the output file (optional)
+*gene_names*:                   dictionary {gene ID: gene name} (optional)
 """
 
 class Usage(Exception):
@@ -20,34 +25,32 @@ class Usage(Exception):
 
 def main(argv = None):
     """
-    Usage: run_deseq.py cond1_file cond2_file transcript_names_file cond1_label cond2_label method assembly_id [output]
+    Usage: run_deseq.py cond1_file cond2_file cond1_label cond2_label method [output] [gene_names]
     """
-    narg = 9
     if argv is None:
         argv = sys.argv[1:]
     try:
-        if len(argv) < 7:
-            raise Usage("run_deseq.py takes at least %d argument." % 7)
+        if len(argv) < 5:
+            raise Usage("run_deseq.py takes at least %d arguments." % 5)
         cond1_file = argv[0]
         with open(cond1_file,'rb') as f:
             cond1 = pickle.load(f)
         cond2_file = argv[1]
         with open(cond2_file,'rb') as f:
             cond2 = pickle.load(f)
-        transcript_names_file = argv[2]
-        with open(transcript_names_file,'rb') as f:
-            transcript_names = pickle.load(f)
-        cond1_label = str(argv[3])
-        cond2_label = str(argv[4])
-        method = str(argv[5])
-        assembly_id = argv[6]
-        output = str(argv[7])
+        cond1_label = str(argv[2])
+        cond2_label = str(argv[3])
+        method = str(argv[4])
+        output = str(argv[5])
+        gene_names_file = argv[6]
+        with open(gene_names_file,'rb') as f:
+            gene_names = pickle.load(f)
 
         optargs = {}
         if not output=="None": optargs['output'] = output
+        if not gene_names=="None": optargs['gene_names'] = gene_names
         
-        result_filename = inference(cond1_label, cond1, cond2_label, cond2, transcript_names,
-                                    method, assembly_id, **optargs)
+        result_filename = inference(cond1_label, cond1, cond2_label, cond2, method, **optargs)
         
         sys.exit(0)
     except Usage, err:
