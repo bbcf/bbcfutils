87e0d993ae24cea41531d11af324b33bf077538c
Julien Delafontaine <julien.delafontaine@epfl.ch>
2011-08-22 13:36:59 +0200

    changes according to rnaseq.py

--- Python/run_deseq.py
+++ Python/run_deseq.py
@@ -10,13 +10,14 @@ import rpy2.robjects.packages as rpackages
 import rpy2.robjects.numpy2ri
 import rpy2.rlike.container as rlc
 
-usage = """run_deseq.py cond1_file cond2_file cond1_label cond2_label method [output] [gene_names]
+usage = """run_deseq.py cond1_file cond2_file cond1_label cond2_label assembly_id target method maplot output
 
-*cond1_file*, *cond2_file*:     arrays of counts
-*cond1_label*, *cond2_label* :  string describing the respective condition
-*method*:                       'normal' or 'blind'
-*output*:                       name of the output file (optional)
-*gene_names*:                   dictionary {gene ID: gene name} (optional)
+*cond1_file*, *cond2_file*      arrays of counts
+*cond1_label*, *cond2_label*    string describing the respective condition
+*assembly_id*                   integer or string describing the assembly (e.g. 'hg19' or 76)
+*method*                        'normal' or 'blind', for DESeq
+*maplot*                        'interactive' - clic to see names - or 'normal' - produces a .png
+*output*                        name of the output pickle file
 """
 
 class Usage(Exception):
@@ -25,32 +26,41 @@ class Usage(Exception):
 
 def main(argv = None):
     """
-    Usage: run_deseq.py cond1_file cond2_file cond1_label cond2_label method [output] [gene_names]
+    Usage: run_deseq.py cond1_file cond2_file cond1_label cond2_label assembly_id target method maplot output
     """
     if argv is None:
         argv = sys.argv[1:]
     try:
         if len(argv) < 5:
             raise Usage("run_deseq.py takes at least %d arguments." % 5)
+        
         cond1_file = argv[0]
         with open(cond1_file,'rb') as f:
             cond1 = pickle.load(f)
+            
         cond2_file = argv[1]
         with open(cond2_file,'rb') as f:
             cond2 = pickle.load(f)
+            
         cond1_label = str(argv[2])
         cond2_label = str(argv[3])
-        method = str(argv[4])
-        output = str(argv[5])
-        gene_names_file = argv[6]
-        with open(gene_names_file,'rb') as f:
-            gene_names = pickle.load(f)
+        assembly_id = argv[4]
+        
+        targ = argv[5]
+        with open(targ,'rb') as f:
+            target = pickle.load(f)
+            
+        method = str(argv[6])
+        maplot = str(argv[7])
+
+        output = str(argv[8])
 
         optargs = {}
-        if not output=="None": optargs['output'] = output
-        if not gene_names=="None": optargs['gene_names'] = gene_names
+        if not maplot=="None": optargs['maplot'] = maplot
         
-        result_filename = inference(cond1_label, cond1, cond2_label, cond2, method, **optargs)
+        result = inference(cond1_label, cond1, cond2_label, cond2, assembly_id, target, method, **optargs)
+        with open(output,"wb") as f:
+            pickle.dump(result,f)
         
         sys.exit(0)
     except Usage, err:
