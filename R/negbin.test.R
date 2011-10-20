fa4fa51a00d0d92016260e189fbba83d74ef191b
Julien Delafontaine <julien.delafontaine@epfl.ch>
2011-10-20 14:23:08 +0200

    new testing dataset; reads input files; applies a neg bin test on each feature

--- R/negbin.test.R
+++ R/negbin.test.R
@@ -1,86 +1,65 @@
 #!/bin/env
-R --vanilla << "EOF" # Pipe all subsequent lines into R.
+## R --vanilla << "EOF" # Pipe all subsequent lines into R.
 
 library(MASS)
 library(lattice)
 library(multcomp)
+library(rjson)
 
+args=commandArgs(trailingOnly = TRUE)
+args
 set.seed(123)
 
 ## Set a fake experiment dataset
-features = c("feat1","feat2","feat3","feat4","feat5","feat6","feat7","feat8","feat9","feat10","feat11","feat12")
-group1 = c(rnegbin(3, 200,2),rnegbin(3,100,1),rnegbin(3,700,2),rnegbin(3,400,3))
-group2 = c(rnegbin(3, 200,0.2),rnegbin(3,100,0.11),rnegbin(3,300,0.7),rnegbin(3,400,0.4))
-group3 = c(rnegbin(3, 700,2),rnegbin(3,400,1),rnegbin(3,200,2),rnegbin(3,100,1))
-data = data.frame(g1=group1,g2=group2,g3=group3, row.names=features)
+n = 30
+features = paste(rep("feat",n), seq(n), sep="")
+g1.1 = rnegbin(n, 200,2)
+g1.2 = rnegbin(n, 200,2)
+g1.3 = rnegbin(n, 200,2)
+g2.1 = rnegbin(n, 100,0.2)
+g2.2 = rnegbin(n, 100,0.2)
+g2.3 = rnegbin(n, 100,0.2)
+g3.1 = rnegbin(n, 700,0.4)
+g3.2 = rnegbin(n, 700,0.4)
+g3.3 = rnegbin(n, 700,0.4)
+data = data.frame(g1.1,g1.2,g1.3,g2.1,g2.2,g2.3,g3.1,g3.2,g3.3, row.names=features)
+write.table(data,"data.txt", sep=",", row.names=T, col.names=T, quote=F)
 
-temp <- as.factor(rep(rep(c(30,30,30,40,40,40),each=2),3))
-treatment <- rep(rep(rep(c("yes", "no"), each=3), 2),3)
-feature <- rep(c("feat1", "feat2", "feat3"), each=12)
-dtf <- data.frame(feature=feature, exprs=exprs,temp=temp, treatment=treatment)
 
-
-## Read real dataset
+## Design matrix
 design = "design.txt"
 contrast = "contrast.txt"
-design <- read.table(design, header=T, row.names=1,  sep=",")
+data = "data.txt"
 
-i = 1
-Y = as.vector(data[i,])
-X = as.matrix(design) # Y = t(X).b
-g = data.frame(Y=t(Y),X=t(X))
+#main <- function(data, design, contrast){
 
-nbmodel = glm.nb(feat1 ~ X.temp + X.treat, data=g)
-
-for (i in length(data[,1])){ # for each feature
-    Y = as.vector(data[i,])
-    X = as.matrix(design)
-    nbmodel = glm.nb(Y ~ t(X))
-}
-
-
-main <- function(data.file, design, contrast){
-
-  featureCount <- read.table(data.file, header=T, row.names=1, sep="\t")
+  data <- read.table(data, header=T, row.names=1, sep=",")
   design <- read.table(design, header=T, row.names=1,  sep=",")
+  design = as.data.frame(t(design))
+  for (i in 1:length(design)){ design[,i] = as.factor(design[,i]) }
 
-  contrast <- read.table(contrast.file, header=F, sep="\t")
-  contrast.matrix <- data.matrix(contrast)
-  model <- compareFactors(featureCount, design, contrast)
+  for (i in 1:length(data[,1])){ # for each feature
+    print(paste("Feature",i))
+    Y = as.data.frame(t(data[i,]))
+    g = cbind(Y,design)
+    cond = colnames(design)
+    f = cond[1]
+    for (c in cond[2:length(cond)]){
+      f = paste(as.name(f),"+",as.name(c))
+    }
+    f = formula(paste(colnames(Y)[1],"~",f))
 
-  result = data.frame()
-  write.table(result, "ouput")
+    nbmodel = glm.nb(f, data=g)
+    print(summary(nbmodel))
+  }
 
-}
+  ##contrast = glht(nbmodel, linfct=mcp(temp="Tukey"))
 
+  ## contrast <- read.table(contrast.file, header=F, sep="\t")
+  ## contrast.matrix <- data.matrix(contrast)
 
-compareFactors <- function(featureCount, design, contrast){
-
-  ## check the design matrix
-
-  ## compute betas
-  nbmodel <- glm.nb(exprs ~ treatment + temp, data=featureCount)
-
-  ## check the contrast matrix
-
-  ## compute contrasts
-  C = glht(nbmodel, linfct=mcp(temp="Tukey"))
-
-  #beta = nbmodel$coef
-  #comparisons = transpose(contrast.matrix) %*% beta
-
-  print(featureCount)
-  print(summary(nbmodel))
-  print(C)
-
-}
-
-
-
-print(bwplot(exprs ~ treatment | temp + feature, dtf))
-
-by(dtf, feature, compareFactors, 1 , 1) #Apply a Function to a Data Frame Split by Factors
-
-
+  #result = data.frame()
+  #write.table(result, "ouput")
+#  }
 
 
