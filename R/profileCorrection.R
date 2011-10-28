ee086b6de4e45ec3af41cf76ff62b890620be048
Marion <marion.leleu@epfl.ch>
2011-10-28 17:05:07 +0200

    add smoothData.R and profileCorrection.R

---
+++ R/profileCorrection.R
@@ -0,0 +1,106 @@
+
+#profileCorrection.R --args infile baitCoord outputFile reportFile name
+
+Args <- commandArgs(TRUE)
+print(length(Args))
+print(Args)
+infile <- Args[1]
+baitcoord <- Args[2]
+curName <- Args[3]
+correctedFile <- Args[4]
+report <- Args[5]
+
+bin.log=function(x) {
+    r1=min(x$start)
+    r2=max(x$end)
+    c((r1+r2)/2,sum(x$val*(x$end-x$start+1))/(1+r2-r1))
+}
+log.na=function(x,b=2){
+    y=rep(NA,length(x))
+    I=which(x>0)
+    y[I]=log(x[I],base=b)
+    y
+}
+unfold=function(x){
+    cbind(unlist(apply(x,1,function(y)y[1]:y[2])),
+          rep(x[,3],x[,2]-x[,1]+1))
+}
+
+
+profileCorrection <- function(fragsFile,baitCoord,plotFile=NA,all=FALSE)
+{
+
+        baitCoordSplit <- unlist(strsplit(gsub("-",":",baitCoord,perl=TRUE),":"))
+        bait=as.numeric(baitCoordSplit[2:3])
+        baitChr=baitCoordSplit[1]
+        bed=read.delim(infile, skip=1,header=F,stringsAsFactors=F)
+	if(ncol(bed)<4){bed=read.delim(infile, skip=1,header=F,stringsAsFactors=F,sep=" ")}
+
+        I.down=which(bed[,1]==baitChr & bed[,2]>bait[2])
+        data.down=data.frame(start=bed[I.down,2]-bait[2],end=bed[I.down,3]-bait[2],val=bed[I.down,4])
+        I.up=which(bed[,1]==baitChr & bed[,3]<bait[1])
+        data.up=data.frame(start=bait[1]-bed[I.up,3],end=bait[1]-bed[I.up,2],val=bed[I.up,4])
+        cuts=round(log.na(data.down$start+data.down$end))
+        binned.down=as.data.frame(t(sapply(split(data.down[which(data.down[,3]>0.0),],cuts[which(data.down[,3]>0.0)]),bin.log)))
+        (lm.down=lm(y~x,data=data.frame(x=log(binned.down[,1]),y=log(binned.down[,2]))))
+        cuts=round(log.na(data.up$start+data.up$end))
+        binned.up=as.data.frame(t(sapply(split(data.up[which(data.up[,3]>0.0),],cuts[which(data.up[,3]>0.0)]),bin.log)))
+        (lm.up=lm(y~x,data=data.frame(x=log(binned.up[,1]),y=log(binned.up[,2]))))
+        a.down=mean(log(binned.down[,2]))+mean(log(binned.down[,1]))
+        a.up=mean(log(binned.up[,2]))+mean(log(binned.up[,1]))
+        cs.down<-1+exp(a.down)/ (0.5*(bed[I.down,2]+bed[I.down,3])-bait[2])
+        d.down<-data.frame("start"=bed[I.down,2],"end"=bed[I.down,3],"score"=bed[I.down,4],"fit"=cs.down,"scoreFit"=bed[I.down,4]/cs.down)
+        cs.up<-1+exp(a.up)/ (bait[1]-0.5*(bed[I.up,2]+bed[I.up,3]))
+        d.up<-data.frame("start"=bed[I.up,2],"end"=bed[I.up,3],"score"=bed[I.up,4],"fit"=cs.up,"scoreFit"=bed[I.up,4]/cs.up)
+        data2=rbind(d.up,d.down)
+        data2=cbind(rep(baitChr,nrow(data2)),data2)
+        colnames(data2)[1]="chromosome"
+
+        if(!is.na(plotFile))
+        {
+                pdf(file=plotFile)
+                plot(data2$start,data2$score,t='h',ylim=c(-max(data2$scoreFit),max(data2$score)),main=paste("corrected profiles\n",plotFile,sep=""),xlab="",ylab="frags score",col="dark grey")
+                points(data2$start,-1*data2$scoreFit,t='h',col="orange")
+                lines(0.5*(data2$start+data2$end),data2$fit,col="red",lty=2) #orangered or orangered1
+                legend("topright",legend=c("before correction","profile corrected","fit"),col=c("darkgrey","orangered","red"),lwd=c(1,1,2))
+                dev.off()
+
+
+                #pdf(file=gsub(".pdf","_fit.pdf",plotFile))
+                pdf(file=plotFile)
+                plot(binned.down[,1],binned.down[,2],log='xy',pch=20,xlab='position',ylab='value',t='b',col='red')
+                a.down=mean(log(binned.down[,2]))+mean(log(binned.down[,1]))
+                lines(binned.down[,1],1+exp(a.down)/binned.down[,1],col='magenta')
+                points(binned.up[,1],binned.up[,2],pch=20,col='blue',t='b')
+                a.up=mean(log(binned.up[,2]))+mean(log(binned.up[,1]))
+                lines(binned.up[,1],1+exp(a.up)/binned.up[,1],col='cyan')
+                legend("topright",legend=c("Down (data)","Up (data)","Down (fit)","Up (fit)"),col=c("red","blue","magenta","cyan"),lty=c(2,2,1,1))
+                title("data vs. fit")
+
+                dev.off()
+        }
+
+	if(all)
+	{	# will return data for all chromosomes
+		I.otherChr <- which(bed[,1] != baitChr)
+		res = bed[I.otherChr,]
+		colnames(res) = colnames(data2)[c(1:3,6)]
+		res <- rbind(res,data2[,c(1:3,6)])
+       		return(res)
+	}
+
+	return(data2[,c(1:3,6)]) #return only profile corrected data (=data on baitChr only)
+}
+
+correctedData=profileCorrection(infile,baitcoord,report,TRUE)
+
+header=paste("track type=bedGraph name='",curName," (profile corrected)' description='",curName," (profile corrected)' visibility=full windowingFunction=maximum",sep="")
+write.table(header,correctedFile,row.names=FALSE,col.names=FALSE,quote=FALSE)
+write.table(cbind(correctedData[,1:3],round(correctedData[,4],2)),file=correctedFile,quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t",append=TRUE)
+
+print("*****************")
+print(paste("Profile Correction of ",infile," done!"))
+print(paste("resfile=",correctedFile))
+print(paste("report file=",report))
+print("*****************")
+
---
+++ R/smoothData.R
@@ -0,0 +1,118 @@
+#smoothData.R --args infile nFragsPerWin curName resfile regToExclude (if any) 
+
+Args <- commandArgs(TRUE)
+print(length(Args))
+print(Args)
+fragsFile <- Args[1]
+nFragsPerWin <- as.numeric(Args[2])
+curName <- Args[3]
+resFile <- Args[4]
+regToExclude <- Args[5]
+
+skipN=1
+
+
+updateFactors <- function(myData)
+{
+	myData[] <- lapply(myData, function(x) x[,drop=TRUE])
+	return(myData)
+}
+
+#getRegions <- function(myData,baitCoord,interRegCoord)
+getRegions <- function(myData,interRegCoord)
+{
+	i_baitChr <- which(myData[,1] == interRegCoord[1])
+	notBaitChr <- updateFactors(myData[which(myData[,1] != interRegCoord[1]),])
+	i <- which(myData[i_baitChr,2] < as.numeric(interRegCoord[2]))
+	regUp <- updateFactors(myData[i_baitChr[i],])
+	i <- which(myData[i_baitChr,3] > as.numeric(interRegCoord[3]))
+	regDown <- updateFactors(myData[i_baitChr[i],])
+	return(list(othersChrs=notBaitChr,upstream_of_interactiveReg=regUp,down_of_interactiveReg=regDown))
+}
+
+mergeLists <- function(x,y)
+{
+	if (length(x) == 0){return(y)}
+    if (length(y) == 0){return(x)}
+	i = intersect(names(y), names(x))
+	j <- setdiff(names(x),names(y))
+	k <- setdiff(names(y),names(x))
+	z <- list()
+	if(length(i)>0){for(n in i){z[[n]] <- rbind(x[[n]],y[[n]])}}
+	if(length(j)>0){for(n in j){z[[n]]<-x[[n]]}}
+	if(length(k)>0){for(n in k){z[[n]]<-y[[n]]}}
+	return(z)
+}
+
+
+splitData <- function(myData)
+{
+	z <- list();
+	for(i in names(table(myData[,1]))){j <- which(myData[,1]==i);z[[i]]<-cbind(myData[j,],rownames(myData)[j])}
+	return(z)
+}
+
+
+groupFrag_betweenIndices <- function(myVector,s,e)
+{
+        return(lapply(1:length(s),function(j){mean(myVector[s[j]:e[j]]) }))
+}
+
+
+generateSmoothedWindows <- function(myData,n)
+{
+        data_byChr <- splitData(myData)
+	if(length(data_byChr)>0){
+        newWindows <- lapply(1:length(data_byChr),
+                function(i){if(nrow(data_byChr[[i]])>0){
+			s=c();e=c();
+			mid=round(n/2);if((n/2)==round(n/2)){nprev=mid-1;nnext=mid}else{nprev=mid;nnext=mid}
+			mid=seq(1,nrow(data_byChr[[i]]),by=1)
+			istart=mid-rep(nprev,length(mid))
+			iend=mid+rep(nnext,length(mid))
+			ipairs <- cbind(istart,iend,mid)
+			ipairs[which(ipairs[,2]>length(mid)),2]=length(mid)
+			ipairs[which(ipairs[,1]<0),1]=1
+                        
+                        cbind(data_byChr[[i]][mid,2],data_byChr[[i]][mid,3],groupFrag_betweenIndices(data_byChr[[i]][,4],ipairs[,1],ipairs[,2]))
+			} #end if nrow>0
+                        } #end function(i) 
+                
+        )
+        names(newWindows)=names(data_byChr)}
+	else{newWindows=c()}
+        return(newWindows)
+}
+
+stringsAsFactors=FALSE
+allFrags <- read.delim(fragsFile,header=FALSE,skip=skipN)
+data <- data.frame("chr"=as.factor(allFrags[,1]),"start"=as.integer(allFrags[,2]),"end"=as.integer(allFrags[,3]),"score"=as.numeric(allFrags[,4]),stringsAsFactors=FALSE)
+allSmoothedWindows <- generateSmoothedWindows(data,nFragsPerWin)
+
+if(nchar(regToExclude)>2)
+{
+	regToExcludeSplit <- unlist(strsplit(gsub("-",":",regToExclude,perl=TRUE),":"))
+	data_splitted <- getRegions(data,regToExcludeSplit)
+	othersChrs_allWindows <- generateSmoothedWindows(data_splitted[[1]],nFragsPerWin)
+	upInteractiveReg_allWindows <- generateSmoothedWindows(data_splitted[[2]],nFragsPerWin)
+	downInteractiveReg_allWindows <- generateSmoothedWindows(data_splitted[[3]],nFragsPerWin)
+	allSmoothedWindows <- mergeLists(othersChrs_allWindows,upInteractiveReg_allWindows)
+	allSmoothedWindows <- mergeLists(allSmoothedWindows,downInteractiveReg_allWindows)
+	header=paste("track type=bedGraph name='",curName," (all smoothed windows - ",nFragsPerWin," fragments per window)' description='",curName," (all smoothed windows - ",nFragsPerWin," fragments per window - region excluded=",regToExclude,")' visibility=full windowingFunction=maximum",sep="")
+}
+
+if(nchar(regToExclude)<=2)
+{
+	header=paste("track type=bedGraph name='",curName," (all smoothed windows - ",nFragsPerWin," fragments per window)' description='",curName," (all smoothed windows - ",nFragsPerWin," fragments per window)' visibility=full windowingFunction=maximum",sep="")
+}
+
+print("Will write file")
+write.table(header,resFile,row.names=FALSE,col.names=FALSE,quote=FALSE)
+lapply(1:length(allSmoothedWindows),function(i){write.table(cbind(rep(names(allSmoothedWindows)[i],nrow(allSmoothedWindows[[i]])),allSmoothedWindows[[i]]),resFile,row.names=FALSE,col.names=FALSE,quote=FALSE,append=TRUE);return(nrow(allSmoothedWindows[[i]]))})
+
+
+print("*****************")
+print(paste("Profile Correction of ",fragsFile," done!"))
+print(paste("resfile=",resFile))
+print("*****************")
+
