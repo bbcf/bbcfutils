4fd00a7dbb3d56596966247382a93863225d2a02
Julien Delafontaine <julien.delafontaine@epfl.ch>
2012-02-10 00:16:47 +0100

    converted to optparse like the others

--- Python/run_gminer.py
+++ Python/run_gminer.py
@@ -3,30 +3,37 @@
 import sys
 import pickle
 import gMiner
+import optparse
 
-usage = """run_gminer.py pickle_file
-
-pickle_file: a pickle file with a dictionary describing the gMiner job,
-saves the output as an entry 'job_output' in the same dictionary, same file.
-"""
 
 class Usage(Exception):
     def __init__(self,  msg):
         self.msg = msg
 
 def main(argv = None):
-    if argv is None:
-        argv = sys.argv[1:]
+    opts = (("-v", "--via", "Run executions using method 'via' (can be 'local' or 'lsf')", {'default': "lsf"}),
+           )
     try:
-        if len(argv) != 1:
-            raise Usage("run_gminer.py takes exactly 1 argument.")
-        pickle_file = argv[0]
+        usage = "run_gminer.py pickle_file [OPTIONS]"
+        desc = """ Runs gMiner [...]
+               Arguments:
+               `pickle_file`: a pickle file with a dictionary describing the gMiner job,
+               saves the output as an entry 'job_output' in the same dictionary, same file. """
+        parser = optparse.OptionParser(usage=usage, description=desc)
+        for opt in opts:
+            parser.add_option(opt[0],opt[1],help=opt[2],**opt[3])
+        (opt, args) = parser.parse_args()
+
+        if len(args) != 1:
+            parser.error("run_gminer.py takes exactly 1 argument.")
+        pickle_file = args[0]
         with open(pickle_file,'r') as f:
             job = pickle.load(f)
         job['job_output'] = gMiner.run(**job)
         with open(pickle_file,'w') as f:
             pickle.dump(job,f)
         sys.exit(0)
+
     except Usage, err:
         print >>sys.stderr, err.msg
         print >>sys.stderr, usage
