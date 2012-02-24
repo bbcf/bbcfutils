8e177a025893576b1ab51a1a596b2ca72c06245f
Marion <marion.leleu@epfl.ch>
2012-02-24 11:01:54 +0100

    mergeBigWig.sh:now returns bw format (needs assembly.name)

--- bash_scripts/mergeBigWig.sh
+++ bash_scripts/mergeBigWig.sh
@@ -2,10 +2,10 @@
 arguments=$*
 echo "arguments: "${arguments}
 
-#mergeBigWig.sh -i file1,file2,file3...filen -o outputfile
-#./mergeBigWig.sh -i density_file_CHAN1_2nM_A_filtered_sorted.bw,density_file_CHAN1_2nM_B_filtered_sorted.bw -o fileres
+#mergeBigWig.sh -i file1,file2,file3...filen -o outputfile -a assembly
+#./mergeBigWig.sh -i density_file_CHAN1_2nM_A_filtered_sorted.bw,density_file_CHAN1_2nM_B_filtered_sorted.bw -o fileres -a hg19
 
-while getopts i:o: name   ## les options acceptant un paramètres sont suivies de ":"
+while getopts i:o:a: name   ## les options acceptant un paramètres sont suivies de ":"
   do
     case $name in
         i)
@@ -16,12 +16,23 @@ while getopts i:o: name   ## les options acceptant un paramètres sont suivies d
             outputFile_flag=1
             outputFile="$OPTARG"
             ;;
+	a)
+	    assembly_flag=1
+	    assembly="$OPTARG"
+	    ;;
         ?)
-            printf "Usage: %s: [-i bigWig files (comma separated)] [-o output filename] args\n" $0
+            printf "Usage: %s: [-i bigWig files (comma separated)] [-o output filename] [-a assembly (if provided result will be in a bigWig format)] args\n" $0
             exit 2
             ;;
     esac
 done
+
+if [[ ! -z "${assembly_flag}" ]]
+then
+	echo "assembly="$assembly
+	echo "will convert output file to bigWig format (assembly="$assembly")"
+	`genrep4humans.py -l -a ${assembly}|cut -f2,3 > chromList.txt`	
+fi
 
 infiles=""
 for x in `echo $bwFiles|awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++){print $i}}'`; do
@@ -44,5 +55,13 @@ tmpfile=$outputFile".tmp"
 #echo "tmpfile="$tmpfile
 `cat $infiles  | awk '{for(i=$2;i<$3;i++){print $1"\t"i"\t"$4}}' | sort -k 1,1 -k2,2n | groupBy -i stdin -grp 1,2 -c 3 -o mean > $tmpfile`
 awk '{if(NR==1){ch=$1;s=$2;e=$2;curScore=$3} if($1==ch && $3==curScore){e=$2}else{e++;print ch"\t"s"\t"e"\t"curScore; ch=$1;s=$2;e=$2;curScore=$3}} END{e++;print ch"\t"s"\t"e"\t"curScore}' $tmpfile  > $outputFile 
-rm $tmpfile
+if [[ ! -z "${assembly_flag}" ]]
+then
+	echo "will convert bedGraph to bw (assembly="$assembly")"
+	`genrep4humans.py -l -a $assembly|cut -f2,3 > chromList.txt` 
+	`wigToBigWig $outputFile chromList.txt $tmpfile`
+	
+fi
+
+#rm $tmpfile
 rm $infiles
