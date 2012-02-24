e813807203a189c83e351f5056af9bc41c264adf
Marion <marion.leleu@epfl.ch>
2012-02-24 11:04:22 +0100

    mergeBigWig.sh:now returns bw format (needs assembly.name)

--- bash_scripts/mergeBigWig.sh
+++ bash_scripts/mergeBigWig.sh
@@ -27,32 +27,18 @@ while getopts i:o:a: name   ## les options acceptant un paramètres sont suivies
     esac
 done
 
-if [[ ! -z "${assembly_flag}" ]]
-then
-	echo "assembly="$assembly
-	echo "will convert output file to bigWig format (assembly="$assembly")"
-	`genrep4humans.py -l -a ${assembly}|cut -f2,3 > chromList.txt`	
-fi
 
 infiles=""
 for x in `echo $bwFiles|awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++){print $i}}'`; do
 #echo "Will transform file $x to bedGraph file"
 `bigWigToWig $x $x".tmp"`
 `grep -v '#bedGraph' $x".tmp" > $x".bedGraph"`
-#`awk '{if($0 !~ /^#bedGraph/){print > FILENAME"_"$1".bedGraph"}}' $x".tmp"`
 infiles=$infiles$x".bedGraph,"
 rm $x".tmp"
 done
 
-#allChr=for x in `cut -f1 density_file_CHAN1_2nM_B_filtered_sorted.bw.bedGraph|uniq`;do echo "curChr="$x;done
-
-
-#./mergeBigWig.sh -i density_file_CHAN1_2nM_A_filtered_sorted.bw,density_file_CHAN1_2nM_B_filtered_sorted.bw -o fileres
-
 infiles=`echo $infiles | sed 's/,/ /g'`
-#echo "infiles="$infiles
 tmpfile=$outputFile".tmp"
-#echo "tmpfile="$tmpfile
 `cat $infiles  | awk '{for(i=$2;i<$3;i++){print $1"\t"i"\t"$4}}' | sort -k 1,1 -k2,2n | groupBy -i stdin -grp 1,2 -c 3 -o mean > $tmpfile`
 awk '{if(NR==1){ch=$1;s=$2;e=$2;curScore=$3} if($1==ch && $3==curScore){e=$2}else{e++;print ch"\t"s"\t"e"\t"curScore; ch=$1;s=$2;e=$2;curScore=$3}} END{e++;print ch"\t"s"\t"e"\t"curScore}' $tmpfile  > $outputFile 
 if [[ ! -z "${assembly_flag}" ]]
@@ -63,5 +49,5 @@ then
 	
 fi
 
-#rm $tmpfile
+rm $tmpfile
 rm $infiles
