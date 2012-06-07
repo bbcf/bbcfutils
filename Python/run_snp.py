39f7e4263fac6b6e46fbf8589beb2965976e5818
Julien Delafontaine <julien.delafontaine@epfl.ch>
2012-06-07 14:48:57 +0200

    bugfix

--- Python/run_snp.py
+++ Python/run_snp.py
@@ -75,7 +75,7 @@ def main(argv = None):
 
         # Program body
         with execution( M, description=hts_key, remote_working_directory=opt.wdir ) as ex:
-            print "Current working directory:", opt.wdir
+            print "Current working directory:", ex.working_directory
             (bam_files, job) = mapseq.get_bam_wig_files(ex, job, minilims=opt.mapseq_limspath,
                                                         hts_url=mapseq_url,
                                                         script_path=gl.get('script_path',''),
@@ -86,6 +86,7 @@ def main(argv = None):
             logfile.write("done\n");logfile.flush()
 
             # Samtools pileup
+            samples = dict((chrom,{}) for chrom in genomeRef.keys()) 
             pileup_dict = dict((chrom,{}) for chrom in genomeRef.keys()) # {'chr.': {}}
             for idGroup,dictRuns in job.groups.iteritems():
                 nbRuns = len(dictRuns["runs"].keys())
@@ -94,23 +95,27 @@ def main(argv = None):
                     sampleName = job.groups[idGroup]['name']
                     if(nbRuns>1):
                         sampleName += "_"+dictRun.get('libname',str(idRun))
-                        debugfile.write("many runs, need statistics\n");debugfile.flush()
+                        debugfile.write("(many runs, need statistics)\n");debugfile.flush()
                     for chrom,ref in genomeRef.iteritems():
                         pileupFilename = common.unique_filename_in()
                         future = snp.sam_pileup.nonblocking( ex, assembly, dictRun["bam"], ref,
                                                              via=opt.via, stdout=pileupFilename )
-                        pileup_dict[chrom][pileupFilename] = (sampleName,future)
+                        pileup_dict[chrom][pileupFilename] = future # {chr: {filename: future}}
+                        samples[chrom][pileupFilename] = sampleName
+            sample_names = [samples[pileup_dict.iterkeys().next()][p] \
+                            for p in pileup_dict.itervalues().next().iterkeys()]
 
             # Get & write results
             formattedPileupFilename = {}
             for chrom, dictPileup in pileup_dict.iteritems():
-                allSNPpos,parameters = snp.allSNPposUniq(dictPileup) # {pos: snp}, (minCoverage, minSNP)
+                allSNPpos,parameters = snp.posAllUniqSNP(dictPileup) # {3021:'A'}, (minCoverage, minSNP)
                 if len(allSNPpos) == 0: continue
                 formattedPileupFilename[chrom] = snp.parse_pileupFile(
-                    dictPileup, allSNPpos, chrom,
+                    samples[chrom], allSNPpos, chrom,
                     minCoverage = parameters[0],
                     minSNP = parameters[1])
-            sample_names = pileup_dict.itervalues().next().values()
+                #with open(formattedPileupFilename[chrom]) as f:
+                #    print f.read()
 
             # Add exon & codon information
 #            output = common.cat(formattedPileupFilename[0],formattedPileupFilename[1:],skip=1)
