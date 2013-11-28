bf0b8184842bee235a2fe0806793511f9f0045f3
Julien Delafontaine <julien.delafontaine@epfl.ch>
2013-11-28 17:07:54 +0100

    tested. More explanations.

--- Python/track.py
+++ Python/track.py
@@ -253,10 +253,10 @@ Fields: %s
 f = 'check'
 usage[f] = usage['all'] %f +" file1 [file2 ...]"
 description[f] = 'Checks that a track file is sorted and well-formatted.'
-opts[f] = (("-t", "--format", "File format, if extension not explicit", {'default':None}),
-           ("", "--co", "Check sorted", {'action':"store_true"}),
-           ("", "--cd", "Check duplicates", {'action':"store_true"}),
-           ("", "--cz", "Check zero size", {'action':"store_true"}),
+opts[f] = (("-t", "--format", "File format, if extension not explicit.", {'default':None}),
+           ("", "--co", "Check that the file is sorted (by chr,start,end).", {'action':"store_true"}),
+           ("", "--cd", "Check that there are no lines duplicated.", {'action':"store_true"}),
+           ("", "--cz", "Check that no region has size zero (start==end).", {'action':"store_true"}),
           )
 
 def check(*args,**kw):
