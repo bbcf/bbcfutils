34c08fc3d716cbc62626f4bde81ca0acb690b6e9
Jacques Rougemont <jacques.rougemont@epfl.ch>
2014-01-08 14:28:21 +0100

    file as list of gene ids

--- Python/genrep4humans.py
+++ Python/genrep4humans.py
@@ -24,7 +24,7 @@ opts = (("-l", "--list", "list available assemblies, or a chromosome table if an
         ("-o", "--output", "output file (default: standard output)", {'default': None}),
         ("-r", "--root", "genrep root directory (default: '/db/genrep/')", {'default': '/db/genrep/'}),
         ("-u", "--url", "url to genrep (default: 'http://bbcf-serv01.epfl.ch/genrep/')",{'default': 'http://bbcf-serv01.epfl.ch/genrep/'}),
-        ("-g", "--genes", "extract coordinates for a comma-separated list of Ensembl ids",{}),
+        ("-g", "--genes", "extract coordinates for a list of Ensembl ids, specified as a comma-separated list or a filename (gene id must be the first word of each line in the file)",{}),
         ("-z", "--all", "extract coordinates of all genes/transcripts/exons (depending on the 'intype')",{'action': "store_true", 'default': False}),
         ("-c", "--convert", "convert bam headers to natural chromosome names",{}))
 
