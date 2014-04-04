39c769005e92054d23be5a3b6a95d4daf4f58d68
Jacques Rougemont <jacques.rougemont@epfl.ch>
2014-04-04 15:58:39 +0200

    new rna-seq read counter, not working yet

---
+++ C/rnacounter.cc
@@ -0,0 +1,163 @@
+#include "rnacounter.h"
+
+inline int countreads( const samtools::bam1_t *b, void *_d )
+{
+    size_t i = 0;
+    if ((b->core.flag & BAM_FUNMAP) == 0) {
+	i = samtools::bam_aux2i(bam_aux_get(b,"NH"));
+	if (i==0) i=1;
+	(*(std::map< int, size_t >*)_d)[i]++;
+    }
+    return 0;
+}
+
+inline int mapreads( const samtools::bam1_t *b, void *_d ) 
+{
+    exon_list *_el = (exon_list*)_d;
+    if (_el->current_pos >= _el->size()) return 0;
+    int exon_end = (*_el)[_el->current_pos].end,
+	ali_pos = b->core.pos;
+    while (exon_end <= ali_pos) {
+	_el->current_pos++;
+	if (_el->current_pos >= _el->size()) return 0;
+	exon_end = (*_el)[_el->current_pos].end;
+    }
+    exon_list::size_type pos2 = _el->current_pos;
+    int exon_start = (*_el)[pos2].start,
+	read_len = b->core.l_qseq, 
+	ali_len = 0;
+    for ( uint32_t j = 0; j < b->core.n_cigar; j++ ) {
+	int op = bam1_cigar(b)[j]&BAM_CIGAR_MASK,
+	    shift = bam1_cigar(b)[j]>>BAM_CIGAR_SHIFT;
+	if (op == BAM_CMATCH || op == BAM_CDEL || op == BAM_CREF_SKIP) {
+	    if (ali_pos < exon_start) {
+		ali_pos = std::min( exon_start, ali_pos+shift );
+		shift = std::max( 0, shift-exon_start+ali_pos );
+	    }
+	    while (ali_pos+shift >= exon_end) {
+		if (op == BAM_CMATCH) ali_len += exon_end-ali_pos;
+		_el->increment( (double)ali_len/(double)read_len, pos2, bam1_strand(b) );
+		ali_pos = exon_end;
+		ali_len = 0;
+		shift -= exon_end-ali_pos;
+		pos2++;
+		if (pos2 >= _el->size()) return 0;
+		exon_start = (*_el)[pos2].start;
+		exon_end = (*_el)[pos2].end;
+		if (ali_pos < exon_start) {
+		    ali_pos = std::min( exon_start, ali_pos+shift );
+		    shift = std::max( 0, shift-exon_start+ali_pos );
+		}
+	    }
+	    ali_pos += shift;
+	    if (op == BAM_CMATCH) ali_len += shift;
+	}
+	if (op == BAM_CINS) ali_len += shift;
+    }
+    if (ali_len < 1) return 0;
+    _el->increment( (double)ali_len/(double)read_len, pos2, bam1_strand(b) );
+    return 0;
+}
+
+
+
+/**************** non-negative least-squares ***************/
+#include <libtsnnls/tsnnls.h>
+int exon_list::nnls_solve( std::string filename, 
+			   std::ios_base::openmode mode=std::ios_base::out ) {
+/***** dim = #exons, cols = #transcripts *****/
+    double norm = 1e3/(double)reads_total;  // normalize to RPKM *= 10^6/rtotal/10^3
+    int dim = size(), cols = txids.size();
+    double *Avals = (double *)(calloc(dim*cols,sizeof(double)));
+    double *bvals = (double *)(calloc(dim,sizeof(double)));
+    for ( int n = 0; n < dim; n++ ) {
+	double exlen = (double)((*this)[n].end-(*this)[n].start);
+	bvals[n] = norm*counts_antisense[n];
+	for ( std::vector< int >::iterator I = (*this)[n].tmap.begin();
+	      I != (*this)[n].tmap.end();
+	      I++ )
+	    Avals[*I+n*cols] = exlen;
+    }
+    taucs_ccs_matrix *A = taucs_construct_sorted_ccs_matrix(Avals, cols, dim);
+    free(Avals);
+    double *xvals = t_lsqr(A, bvals);
+    if ( xvals > 0 ) {
+	print( filename, xvals, mode );
+	free(xvals);
+	return 0;
+    } else {
+	std::cerr << "NNLS failed\n";
+	return 22;
+    }
+}
+
+int main( int argc, char **argv )
+{
+    int err;
+    options opts(argc,argv,&err);
+    if (err > 0) return err;
+    samtools::samfile_t *_bam = samtools::samopen( opts.bamfile.c_str(), "rb", 0 );
+    if ( !_bam ) {
+	std::cerr << "Could not open " << opts.bamfile << "\n";
+	return 10;
+    }
+    samtools::bam_index_t *_bai = samtools::bam_index_load( opts.bamfile.c_str() );
+    if ( !_bai ) {
+	std::cerr << "Building index of " << opts.bamfile << "\n";
+	samtools::bam_index_build( opts.bamfile.c_str() );
+	_bai = samtools::bam_index_load( opts.bamfile.c_str() );
+    }
+    exon_list exl;
+    exl.stranded = opts.stranded;
+// ---------- count number of reads
+    std::map< int, size_t > _ntags;
+    samtools::bam1_t *b = ((samtools::bam1_t*)calloc(1, sizeof(samtools::bam1_t)));
+    while ( samtools::bam_read1( _bam->x.bam, b ) > 0 ) countreads( b, &_ntags );
+    exl.update_total( &_ntags );
+
+// ---------- parse bed by chromosome and get corresponding reads
+    std::ifstream bedfile( opts.bedfile.c_str() );
+    if ( !bedfile.is_open() ) {
+	std::cerr << "Could not open " << opts.bedfile << "\n";
+	return 11;
+    }
+    std::string oneline;
+    std::getline( bedfile, oneline );
+    int chromid, start, end;
+    std::ios_base::openmode mode = std::ios_base::out;
+    while ( !bedfile.eof() ) {
+	if (exl.append(oneline, opts.chrom)) {
+	    std::cout << exl.chrom << "\n";
+	    samtools::bam_parse_region( _bam->header, exl.chrom.c_str(), 
+					&chromid, &start, &end );
+	    if (chromid < 0) {
+		std::cerr << exl.chrom << " not found in " << opts.bamfile << "\n";
+		return 12;
+	    }
+	    exl.reset();
+	    samtools::bam_fetch( _bam->x.bam, _bai, chromid, 
+				 0, (int)_bam->header->target_len[chromid],
+				 &exl, mapreads );
+	    int err = exl.nnls_solve( opts.outfile, mode );
+	    if (err > 0) return err;
+	    mode = std::ios_base::app;
+	    exl.next();
+	}
+	std::getline( bedfile, oneline );
+    }
+    bedfile.close();
+    samtools::bam_parse_region( _bam->header, exl.chrom.c_str(), 
+				&chromid, &start, &end);
+    if (chromid < 0) {
+	std::cerr << exl.chrom << " not found in " << opts.bamfile << "\n";
+	return 12;
+    }
+    exl.reset();
+    samtools::bam_fetch( _bam->x.bam, _bai, chromid, 
+			 0, (int)_bam->header->target_len[chromid],
+			 &exl, mapreads );
+    exl.nnls_solve( opts.outfile, mode );
+    samtools::bam_index_destroy( _bai );
+    samtools::samclose( _bam );
+    return 0;
+}
---
+++ C/rnacounter.h
@@ -0,0 +1,181 @@
+/***************** STL *************************/
+#include <map>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstdio>
+/*************** Cmd Line parser ***************/
+#include <tclap/CmdLine.h>
+/************** samtools: own namespace *******************/
+typedef unsigned int uint32_t;
+namespace samtools {
+#include <samtools/sam.h>
+}
+
+typedef struct { int start; int end; std::vector< int > tmap; bool revstrand; } exon;
+    
+class exon_list : public std::vector< exon > {
+
+public:
+// ------ constructor: initialize member vars
+    void update_total( std::map< int, size_t >* );
+    bool append( std::string, std::string );
+    void increment( double, size_type, bool );
+    void reset() {
+	counts_sense.clear(); counts_antisense.clear(); counts_both.clear();
+	if (stranded) {
+	    counts_sense.resize(size(),0.0); counts_antisense.resize(size(),0.0);
+	} else counts_both.resize(size(),0.0);
+	current_pos = 0;
+    }
+    void next();
+    int nnls_solve( std::string, std::ios_base::openmode );
+    void print( std::string, double*, std::ios_base::openmode );
+    size_type current_pos;
+    bool stranded;
+    std::string chrom;
+
+private:
+    long reads_total;
+    std::vector< double > counts_sense;
+    std::vector< double > counts_antisense;
+    std::vector< double > counts_both;
+    std::map< std::string, int > txids;
+    std::string next_chrom, next_name;
+    exon next_exon;
+
+};
+
+inline void exon_list::update_total( std::map< int, size_t > *_n ) {
+    for ( std::map< int, size_t >::const_iterator I = _n->begin(); 
+	  I != _n->end();
+	  I++ ) reads_total += I->second/I->first;
+    _n->clear();
+}
+
+inline void exon_list::increment( double x, size_type index, bool read_rev ) {
+/* DEBUG   x = 1; */
+    if (stranded) {
+// ----------- read/exon strand mismatch:
+	if (read_rev ^ (*this)[index].revstrand)
+	    counts_antisense[index] += x;
+	else 
+	    counts_sense[index] += x;
+    } else counts_both[index] += x;
+}
+
+inline bool exon_list::append( std::string line, std::string chr_select ) {
+    exon parsed;
+    std::string dummy, name, _chr;
+    std::stringstream ss(line);
+    getline(ss, _chr, '\t');
+    if (_chr.compare(chr_select) != 0) return false;
+    getline(ss, dummy, '\t');
+    parsed.start = atoi(dummy.c_str());
+    getline(ss, dummy, '\t');
+    parsed.end = atoi(dummy.c_str());
+    getline(ss, name, '\t');
+    getline(ss, dummy, '\t');
+    parsed.revstrand = (dummy[0] == '-');
+    std::map< std::string, int >::iterator Im;
+    if (chrom.empty() || chrom.compare(_chr) == 0) {
+	if (chrom.empty()) chrom = _chr;
+	std::stringstream ss2(name);
+	while (!ss2.eof()) {
+	    getline(ss2, dummy, '|');
+	    Im = txids.find(dummy);
+	    if (Im == txids.end()) 
+		Im = txids.insert(Im, std::make_pair(dummy, txids.size()));
+	    parsed.tmap.push_back(Im->second);
+	}
+	push_back( parsed );
+	return false;
+    }
+    next_chrom = _chr;
+    next_exon = parsed;
+    next_name = name;
+    return true;
+}
+
+inline void exon_list::next() {
+    clear();
+    txids.clear();
+    chrom = next_chrom;
+    std::stringstream ss2(next_name);
+    std::string dummy;
+    int m = 0;
+    while (!ss2.eof()) {
+	getline(ss2, dummy, '|');
+	txids[dummy] = m;
+	next_exon.tmap.push_back(m);
+	m++;
+    }
+    push_back( next_exon );
+}
+
+inline void exon_list::print( std::string filename, double *x, 
+			      std::ios_base::openmode mode=std::ios_base::out ) {
+    std::ofstream outfile;
+    std::ostream* outstr = &std::cout;
+    if (filename.size()) {
+	outfile.open( filename.c_str(), mode );
+	if (outfile.is_open()) outstr = &outfile;
+	else std::cerr << "Could not open " << filename
+		       << ", writing to stdout\n";
+    }
+    for ( std::map< std::string, int >::iterator Im = txids.begin();
+	  Im !=  txids.end(); Im++ )
+	(*outstr) << Im->first << '\t' << x[Im->second] << "\n";
+    if (outfile.is_open()) outfile.close();
+}
+
+/*********************** Command-line options ******************/
+
+class options {
+
+public:
+    options( int, char**, int* );
+    bool stranded;
+    std::string bamfile, bedfile, outfile, chrom;
+
+};
+
+options::options( int argc, char **argv, int *err ) {
+/********
+Add multiple chromosomes (or all), 
+write exon-level (bed) output
+ ********/
+    try {
+	TCLAP::CmdLine cmd( "Count reads on transcripts from a genome-level bam file and a non-overlapping set of exons." );
+	TCLAP::ValueArg< std::string > of( "o", "outptut", "Output file (default stdout)", false, "", "string" );
+	cmd.add( of ); 
+	TCLAP::ValueArg< std::string > bf( "b", "bamfile", "Bam file", true, "", "string" );
+	cmd.add( bf ); 
+	TCLAP::ValueArg< std::string > ef( "e", "exonfile", "Exon bed file", false, "", "string" );
+	cmd.add( ef ); 
+	TCLAP::ValueArg< std::string > ch( "c", "chromosome", "Chromosome name", false, "", "string" );
+	cmd.add( ch );
+	TCLAP::SwitchArg st( "s", "stranded", "Compute sense and antisense reads separately",  false );
+	cmd.add( st );
+
+	cmd.parse( argc, argv );
+
+	stranded = st.getValue();
+	bamfile = bf.getValue();
+	bedfile = ef.getValue();
+	outfile = of.getValue();
+	chrom = ch.getValue();
+// DEBUG
+	stranded = true;
+	bamfile = "/scratch/cluster/monthly/jrougemo/test_rnaQ/Cviwt1_filtered.bam";
+	bedfile = "/scratch/cluster/monthly/jrougemo/test_rnaQ/counter/TAIR10_unique_exons.txt";
+	outfile = "/scratch/cluster/monthly/jrougemo/test_rnaQ/counter/test_out.txt";
+	chrom = "1";
+	*err = 0;
+    } catch( TCLAP::ArgException &e ) {
+	std::cerr << "Error: " << e.error() << " " << e.argId() << "\n";
+	*err = 10;
+    }
+}
