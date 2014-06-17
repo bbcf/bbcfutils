c802063e5349b59c1697c345ef86c536be54713e
Jacques Rougemont <jacques.rougemont@epfl.ch>
2014-06-17 16:02:50 +0200

    that was not the median...

--- R/bamQC.R
+++ R/bamQC.R
@@ -8,7 +8,9 @@ title = args[3]
 data = read.delim(file('stdin'), header=FALSE)
 freqs = sapply(split(data[,3]-data[,2],data[,4]),sum)
 freqs0 = gsize-sum(freqs)
-med = median(freqs)
+qtiles = freqs0+cumsum(freqs)
+med = as.integer(names(freqs)[max(which(qtiles < gsize/2 ))])
+avg = round(sum(as.numeric(names(freqs))*freqs)/gsize,2)
 
 pdf(pdfname)
 par(cex=1.3,lwd=1.5,las=1,mar=c(5,5,5,1)+.1,mgp=c(4,1,0))
@@ -17,7 +19,8 @@ plot(as.integer(names(freqs)),freqs,log='xy',pch=20,t='b',ylim=c(1,gsize*1.5),
 abline(h=c(freqs0,gsize),col=c('blue','red'))
 text(2,freqs0,paste("Uncovered positions:",freqs0),pos=1,col='blue',cex=.8)
 text(2,gsize,paste("Genome size:",gsize),pos=3,col='red',cex=.8)
-abline(v=med,col='cyan',lty=2)
+abline(v=c(avg,med),col='cyan',lty=2)
 text(med,1,paste("Median:",med),pos=2,col='cyan',cex=.8)
+text(avg,5,paste("Average:",avg),pos=2,col='cyan',cex=.8)
 dev.off()
 
