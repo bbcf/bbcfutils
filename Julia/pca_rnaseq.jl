31cb4a8ac1e38eebb1f33d0fa59b2dc522031e09
Julien Delafontaine <julien.delafontaine@epfl.ch>
2014-12-11 11:09:49 +0100

    new PCA version: PDF backend, MultivariateStats package replaces DimensionalityReduction

--- Julia/pca_rnaseq.jl
+++ Julia/pca_rnaseq.jl
@@ -1,7 +1,8 @@
 #!/usr/bin/env julia
 
-using DimensionalityReduction
+using MultivariateStats
 using DataFrames
+using Gadfly
 
 
 if length(ARGS) > 0; filename = ARGS[1]
@@ -17,27 +18,26 @@ header = map(string,names(df))
 rpkm_cols = filter(i->ismatch(r"^rpkm.*",header[i]), 1:length(header))
 rpkm_names = [h[6:end] for h in header[rpkm_cols]]
 rpkm = convert(Array,DataArray(df[rpkm_cols]))
-trpkm = transpose(log(rpkm.+0.5))
-p = pca(trpkm, scale=false)
+trpkm = log(rpkm.+0.5)
+p = fit(PCA, trpkm, maxoutdim=10)
+proj = trpkm' * p.proj
 
 
 # Biplot
-using Gadfly
-pl = plot(x=p.scores[:,1],y=p.scores[:,2], Geom.point,
+pl = plot(x=proj[:,1],y=proj[:,2], Geom.point,
           Guide.xlabel("PC1"), Guide.ylabel("PC2"),
           Guide.title("PCA biplot of samples ~ gene expression (log RPKM)"),
           label=rpkm_names, Geom.label(hide_overlaps=false) )
-draw(PNG("$(outprefix).png", 6inch, 6inch), pl)
-draw(D3("$(outprefix).js", 6inch, 6inch), pl)
+draw(PDF("$(outprefix).pdf", 6inch, 6inch), pl)
 
 
 # Barplot of variance proportions
-cumvar = string(round(p.cumulative_variance[2]*100.,2))
-barplot = plot(x=1:length(p.proportion_of_variance), y=p.proportion_of_variance, Geom.bar(position=:dodge),
+vars = p.prinvars / p.tvar
+cumvar = string(round(sum(vars[1:2])*100.,2))
+barplot = plot(x=1:length(vars), y=vars, Geom.bar(position=:dodge),
                Theme(bar_spacing=0.5cm), Scale.x_discrete,
-               label=["PC1+PC2: $cumvar %"], Geom.label,
+               Guide.title("PC1+PC2: $cumvar %"),
                Guide.xlabel("PC#"), Guide.ylabel("Proportion of variance") )
-draw(PNG("$(outprefix)_sdev.png", 6inch, 6inch), barplot)
-draw(D3("$(outprefix)_sdev.js", 6inch, 6inch), barplot)
+draw(PDF("$(outprefix)_sdev.pdf", 6inch, 6inch), barplot)
 
 
